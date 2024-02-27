9052fdd764698c6bc6103dedfdc2b7f2ac3ebb3f
anastasiadergaeva <adergaeva@gmail.com>
2024-02-27 14:24:57 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import './App.css';
+import Header from './components/Header/Header.jsx';
 
 function App() {
+    return (
+        <Header />
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.scss'
+import logo from '../../images/german.png'
+console.log(logo);
+export default function Header() {
+    return (
+        <div className="header">
+            <div className='header__logo'>
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+            <div className="header__links">
+                <div className='link'>word list</div>
+                <div className='link'>my words</div>
+                <div className='link'>topics</div>
+            </div>
+        </div>
+    )
+}
