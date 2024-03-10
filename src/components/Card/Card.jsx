c0bbab9bb82c935dba79938ac55cd235358f242a
anastasiadergaeva <adergaeva@gmail.com>
2024-03-10 23:38:34 +0300

    add translation button to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,29 @@
+import React, { useState } from 'react';
 import Button from '../Button/Button';
 import './Card.scss';
 
-export default function Card({ german, transcription }) {
+export default function Card({ german, transcription, english }) {
+    const [isTranslated, setIsTranslated] = useState(false);
+    function handleTranslate(event) {
+        switch (event.target.innerText) {
+            case 'translate':
+                return setIsTranslated(true);
+            default:
+                console.log('ne srabotalo');
+        }
+    }
+
     return (
-        <div className="card">
+        <div className="card" onClick={handleTranslate}>
             <p className='card__word'>{german}</p>
             <p className='card__transcription'>{transcription}</p>
-            <Button text='translate' />
+            {
+                isTranslated ? (
+                    <p className='card__translation'>{english}</p>
+                ) : (
+                    <Button text='translate' />
+                )
+            }
         </div>
     )
 }
