84f5adaab1de9879bf8d0c1ba308f354edd07f30
anastasiadergaeva <adergaeva@gmail.com>
2024-03-10 20:35:35 +0300

    create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import hearts from '../../images/hearts.png';
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__thanks">
+                <div className="image">
+                    <img class="image-item" src={hearts} alt="hearts" />
+                </div>
+                <p className='thankstext'>thank you for using my app</p>
+            </div>
+            <div className="footer__link">
+                <p className='describing-link'>my github</p>
+                <div className="link"></div>
+            </div>
+        </footer>
+    )
+}
