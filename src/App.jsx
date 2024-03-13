66b3c2c7bd80dc418f4af1b68d28216331154570
anastasiadergaeva <adergaeva@gmail.com>
2024-03-13 22:01:59 +0300

    rename app component

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+// import WordListPage from './components/WordListPage/WordListPage';
+// import TrainMode from './components/TrainMode/TrainMode.jsx';
+import Slider from './components/Slider/Slider.jsx';
+import './main.scss';
+import data from './data/words.json';
+
+function App() {
+    const example = data.map(topic => (topic.words))
+    console.log(example);
+    return (
+        <>
+            <Header />
+            <div className='main'>
+                <Slider words={example} />
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
