8558128d05a34afb7442fadceb539a2cbf61a24e
anastasiadergaeva <adergaeva@gmail.com>
2024-03-16 13:47:25 +0300

    create mainpage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,32 +1,34 @@
 import Button from '../Button/Button';
-import WordListItem from '../WordListItem/WordListItem';
+import data from '../../data/words.json';
 import styles from './MainPage.module.scss';
+import Slider from '../Slider/Slider';
+
 export default function MainPage() {
+    const dataWords = data.map(topic => (topic.words));
+    const selectedWords = [];
+    dataWords.forEach(elem => {
+        const selected21 = elem.find(item => item.id === 21);
+        const selected22 = elem.find(item => item.id === 22);
+        if (selected21 && selected22) {
+            selectedWords.push(selected21);
+            selectedWords.push(selected22);
+        }
+    });
+
     return (
         <div className={styles.mainpage}>
             <h1 className={styles.mainpage__header}>hello everyone!</h1>
             <p className={styles.mainpage__text}>Let's start learning German! Say Hello first!</p>
-            <div className={styles.mainpage__rows}>
-                <WordListItem
-                    german='hallo'
-                    transcription='[ˈhalo:]'
-                    english='hello'
-                />
-                <WordListItem
-                    german='zusammen'
-                    transcription='[tsuˈzamən]'
-                    english='everyone'
-                />
+            <div className={styles.mainpage__cards}>
+                <Slider
+                    words={selectedWords} />
             </div>
-            <div>
-                <Button text='learn now' />
-            </div>
-            <div className={styles.additionalinfo}>
-                <div>
+            <div className={styles.mainpage__additionalinfo}>
+                <div className={styles.redirect__wordlist}>
                     <p>You can also select words from a word list</p>
                     <Button text='go to the word list' />
                 </div>
-                <div>
+                <div className={styles.redirect__topics}>
                     <p>Or learn words by topic</p>
                     <Button text='go to the topics' />
                 </div>
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import styles from './Slider.module.scss';
 import Card from '../Card/Card';
-import rightArrow from '../../images/right-arrow.png';
-import leftArrow from '../../images/left-arrow.png';
+import EastRounded from '@mui/icons-material/EastRounded';
+import KeyboardBackspaceRounded from '@mui/icons-material/KeyboardBackspaceRounded';
 
 export default function Slider({ words }) {
     const [index, setIndex] = useState(0);
     const [isCardTranslated, setIsCardTranslated] = useState(false);
 
-    const arrayy = words[0].concat(words[1]);
+    function mergeArrays(words) {
+        if (words.length > 1) {
+            return words.reduce((acc, val) => acc.concat(val), []);
+        } else {
+            return words[0];
+        }
+    }
+
+    const mergedArray = mergeArrays(words);
 
     const handleNext = () => {
-        if (index < arrayy.length - 1) {
+        if (index < mergedArray.length - 1) {
             setIndex(index + 1);
             setIsCardTranslated(false);
         }
@@ -27,18 +35,18 @@ export default function Slider({ words }) {
     return (
         <div className={styles.slider}>
             <div onClick={handlePrev} className={styles.slider__arrow}>
-                <img src={leftArrow} alt="previous" className={styles.arrow} />
+                <KeyboardBackspaceRounded />
             </div>
             <Card
-                key={arrayy.id}
-                german={arrayy[index].german}
-                transcription={arrayy[index].transcription}
-                english={arrayy[index].english}
+                key={mergedArray.id}
+                german={mergedArray[index].german}
+                transcription={mergedArray[index].transcription}
+                english={mergedArray[index].english}
                 isTranslated={isCardTranslated}
                 setIsTranslated={setIsCardTranslated}
             />
             <div onClick={handleNext} className={styles.slider__arrow}>
-                <img src={rightArrow} alt="next" className={styles.arrow} />
+                <EastRounded />
             </div>
         </div>
     );
